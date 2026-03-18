10c6425d8cdd51abb0492038ab1959d10d9f8308
lndsilva <lndsilva@hotmail.com>
2026-03-17 22:08:45 -0300

    folha de pagamento

--- folha_pagamento.php
+++ folha_pagamento.php
@@ -1,12 +1,38 @@
 <?php 
 
-	echo "Salário bruto: R$ <br/>";
-	echo "Imposto de renda: <br/>";
+	$salBruto = 5000;
+	
 
-	echo "Porcentagem de desconto: %<br/>";
-	echo "Valor do desconto: R$ <br/>";
+	printf("Salário bruto: R$ %.2f <br/>",$salBruto);
+	
 
-	echo "Salário líquido: R$ <br/>";
+
+	if ($salBruto <= 2428.80) {
+		$vDesconto = 0;
+	}
+	if($salBruto >= 2428.01 && $salBruto <= 2826.65){
+		$vDesconto = 7.5;
+	}
+	if($salBruto >= 2826.66 && $salBruto <= 3751.05){
+		$vDesconto = 12.5;
+	}
+	if($salBruto >= 3751.06 && $salBruto <= 4664.68){
+		$vDesconto = 22.5;
+	}
+	if($salBruto > 4664.68){
+		$vDesconto = 27.5;
+		$valorDescontado = ($salBruto * $vDesconto /100);
+		$salLiquido = $salBruto - $valorDescontado;
+
+	}
+
+	print("<br/>");
+	print("Tabela do imposto de renda <br/>");
+	print("<br/>");
+	print("Porcentagem de desconto: $vDesconto %<br/>");
+	printf("Valor do desconto: R$ %.2f <br/>", $valorDescontado);
+
+	printf("Salário líquido: R$ %.2f", $salLiquido);
 
 
  ?>
