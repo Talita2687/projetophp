967d9829d077b078d11d9df815e93c0a4c6c0405
lndsilva <lndsilva@hotmail.com>
2026-03-17 21:35:13 -0300

    exercício folha de pagamento

---
+++ folha_pagamento.php
@@ -0,0 +1,12 @@
+<?php 
+
+	echo "Salário bruto: R$ <br/>";
+	echo "Imposto de renda: <br/>";
+
+	echo "Porcentagem de desconto: %<br/>";
+	echo "Valor do desconto: R$ <br/>";
+
+	echo "Salário líquido: R$ <br/>";
+
+
+ ?>
